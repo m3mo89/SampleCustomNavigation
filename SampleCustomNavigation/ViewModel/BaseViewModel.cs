77db83e7642e1cb85020e13795e4f9168122de65
Guillermo Tinoco <m3mo89@gmail.com>
2018-06-01 18:07:07 -0500

    Add properties to base view model

--- SampleCustomNavigation/ViewModel/BaseViewModel.cs
+++ SampleCustomNavigation/ViewModel/BaseViewModel.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace SampleCustomNavigation.ViewModel
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
+        private Color _searchExpandedBackground;
+        private string _searchPlaceHolder;
+
         protected BaseViewModel()
         {
 
+        }
+
+        public Color SearchExpandedBackground
+        {
+            get
+            {
+                return _searchExpandedBackground;
+            }
+            set
+            {
+                if (_searchExpandedBackground != value)
+                {
+                    _searchExpandedBackground = value;
+                    OnPropertyChanged(nameof(SearchExpandedBackground));
+                }
+            }
+        }
+
+        public string SearchPlaceHolder
+        {
+            get
+            {
+                return _searchPlaceHolder;
+            }
+            set
+            {
+                if (_searchPlaceHolder != value)
+                {
+                    _searchPlaceHolder = value;
+                    OnPropertyChanged(nameof(SearchPlaceHolder));
+                }
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
