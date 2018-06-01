a4462a84ef946791357ac8aaaa0fe0f25fbee019
Guillermo Tinoco <m3mo89@gmail.com>
2018-06-01 18:07:23 -0500

    Add a simple model

---
+++ SampleCustomNavigation/Data/Item.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SampleCustomNavigation.Data
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
