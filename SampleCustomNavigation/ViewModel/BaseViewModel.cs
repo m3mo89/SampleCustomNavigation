13f0e408aa6aed51ed814657df12d6e70162d553
Guillermo Tinoco <m3mo89@gmail.com>
2018-06-01 15:32:24 -0500

    Add Viewmodel Base

---
+++ SampleCustomNavigation/ViewModel/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SampleCustomNavigation.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        protected BaseViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
